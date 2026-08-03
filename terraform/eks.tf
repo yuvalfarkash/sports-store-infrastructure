b541e0fa9fd732986682f8587a1328843f87fc9c
yuvalfarkash <yuvalfarkash85@gmail.com>
2026-08-03 15:14:45 +0300

    chore: update instance type to t3.medium

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -31,7 +31,7 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 3

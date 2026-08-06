74a308475667a05592af2f7f5db2c5e5954562fd
yuvalfarkash <yuvalfarkash85@gmail.com>
2026-08-06 19:24:55 +0300

    Add metrics-server Helm release

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -173,3 +173,12 @@ resource "helm_release" "argocd_image_updater" {
     helm_release.argocd
   ]
 }
+
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.12.1"
+  depends_on = [module.eks]
+}
